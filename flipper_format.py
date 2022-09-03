d623c044e5fc3c42b7a2f4a70ee6f2726e8233fd
darmiel <71837281+darmiel@users.noreply.github.com>
2022-09-03 02:15:17 +0200

    initial commit

---
+++ flipper_format.py
@@ -0,0 +1,94 @@
+"""
+minimal implementation of FlipperFormat
+"""
+
+from typing import Union, List
+
+class EOFException(Exception): pass
+class NotAPair(Exception): pass
+
+class FlipperFormat:
+    file_name: str
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.fd = open(self.file_name, "r", encoding="UTF-8")
+
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        print("exiting")
+        self.fd.close()
+
+    def close(self):
+        self.fd.close()
+
+    # ------------------------------------------------------------
+
+    def rewind(self) -> None:
+        self.fd.seek(0)
+    
+    def _read_next_line_pair(self) -> List[str]:
+        line = self.fd.readline()
+        if not line: # EOL
+            raise EOFException()
+        # ignore comments or empty lines
+        # if we want to read a key:value pair, the line should contain a ":"
+        if line.startswith("#") or line.strip() == "" or not ":" in line:
+            raise NotAPair()
+        return [z.strip() for z in line.split(":", 1)]
+
+    def read_next_pair(self) -> List[str]:
+        while True:
+            try:
+                return self._read_next_line_pair()
+            except NotAPair:
+                continue
+
+    def read_str(self, key: str) -> str:
+        while True:
+            k, v = self.read_next_pair()
+            if k == key: 
+                return v
+
+    def read_int(self, key: str) -> int:
+        return int(self.read_str(key))
+
+    def read_hex(self, key: str) -> int:
+        return int(self.read_str(key), base=16)
+
+    def read_hex_multi(self, key: str) -> List[int]:
+        return [int(z, base=16) for z in self.read_str(key).split()]
+
+    def read_float(self, key: str) -> Union[float, None]:
+        return float(self.read_str(key))
+
+    def count_subsequent_keys(self, key: str) -> int:
+        count = 0
+        pos = self.fd.tell()
+        while True:
+            try:
+                k, _ = self._read_next_line_pair()
+                if k != key:
+                    break
+            except EOFException:
+                break
+            except NotAPair:
+                continue
+            count += 1
+        self.fd.seek(pos)
+        return count
+
+def marshal(a: dict) -> str:
+    r = ""
+    f = True
+    for k, v in a.items():
+        if f:
+            f = False
+        else:
+            r += "\n"
+        if type(v) != str:
+            v = str(v)
+        r += f"{k}: {v}"
+    return r
---
+++ flipper_format_ir.py
@@ -0,0 +1,124 @@
+from typing import List, Union
+from flipper_format import EOFException, FlipperFormat, marshal
+
+MAX_DATA_PER_LINE = 50
+
+def _to_hex_str(nums: list, sep: str = ' ') -> str:
+    return sep.join([hex(z)[2:].zfill(2) for z in nums]).upper()
+
+class BaseSignal:
+    def __init__(self, is_raw: bool, name: str) -> None:
+        self.is_raw = is_raw
+        self.name = name
+
+class RawSignal(BaseSignal):
+    """
+    name: POWER
+    type: raw
+    frequency: 38000
+    duty_cycle: 0.330000
+    data: 8437 4188 538 1565 539 1565 539 513 544 508 538 513 544 1559 545
+    """
+
+    def __init__(self, name: str, frequency: int, duty_circle: float, data: list) -> None:
+        super().__init__(True, name)
+        self.frequency = frequency
+        self.duty_circle = duty_circle
+        self.data = data
+   
+    def __str__(self) -> str:
+        r = marshal({
+            "name": self.name,
+            "frequency": self.frequency,
+            "duty_circle": self.duty_circle,
+        })
+        for i in range(0, len(self.data), MAX_DATA_PER_LINE):
+            pass
+            r += f"\ndata: {' '.join([str(z) for z in self.data[i:i + MAX_DATA_PER_LINE]])}"
+        return r
+
+    def __hash__(self) -> int:
+        return hash(f"{self.frequency}@{self.duty_circle}::{'-'.join([str(u) for u in self.data])}".replace(" ", "#").lower())
+    
+class ParsedSignal(BaseSignal):
+    """
+    name: VOL-
+    type: parsed
+    protocol: NEC
+    address: 00 00 00 00
+    command: 15 00 00 00
+    """
+
+    def __init__(self, name: str, protocol: str, address: List[int], command: List[int]) -> None:
+        super().__init__(False, name)
+        self.protocol = protocol
+        self.address = address
+        self.command = command
+
+    def __str__(self) -> str:
+        return marshal({
+            "name": self.name,
+            "protocol": self.protocol,
+            "address": _to_hex_str(self.address),
+            "command": _to_hex_str(self.command)
+        })
+
+    def __hash__(self) -> int:
+        return hash(f"{_to_hex_str(self.command)}@{_to_hex_str(self.address)}::{self.protocol}".replace(" ", "#").lower())
+
+def _parse_raw(fff: FlipperFormat, name: str) -> RawSignal:
+    freq = fff.read_int("frequency")
+    dc = fff.read_float("duty_cycle")
+    data = []
+
+    while True:
+        pos = fff.fd.tell()
+        try:
+            k, v = fff.read_next_pair()
+            if k != "data":
+                fff.fd.seek(pos)
+                break
+            data.extend([int(z) for z in v.split()])
+        except EOFException:
+            break
+
+    return RawSignal(name, frequency=freq, duty_circle=dc, data=data)
+
+def _parse_parsed(fff: FlipperFormat, name: str) -> ParsedSignal:
+    protocol = fff.read_str("protocol")
+    address = fff.read_hex_multi("address")
+    command = fff.read_hex_multi("command")
+    return ParsedSignal(name, protocol=protocol, address=address, command=command)
+
+def read_ir(fff: FlipperFormat) -> List[Union[RawSignal, ParsedSignal]]:
+    while True:
+        try:
+            name = fff.read_str("name")
+        except EOFException:
+            break
+
+        typ = fff.read_str("type")
+        if typ == "raw":
+            r = _parse_raw(fff, name)
+        elif typ == "parsed":
+            r = _parse_parsed(fff, name)
+        else:
+            raise Exception(f"unknown signal type '{typ}'")
+
+        yield r
+
+if __name__ == "__main__":
+    print("reading")
+    fff = FlipperFormat("audio.ir")
+
+    hashes = {}
+    for resp in read_ir(fff):
+        print(resp)
+        h = hash(resp)
+        if not h in hashes: hashes[h] = [resp] 
+        else: 
+            hashes[h].append(resp)
+            if len(hashes[h]) > 1:
+                print(">> DUPLICATE")
+        print("#")
+    fff.close()
