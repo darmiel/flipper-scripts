fdea7e2e47f8de29c77317bc3f689653ea5b0f01
Daniel <71837281+darmiel@users.noreply.github.com>
2023-01-09 03:15:24 +0100

    refactor: generate sleep

--- subghz/generate_sleep.py
+++ subghz/generate_sleep.py
@@ -1,10 +1,9 @@
 """ Script to generate a blank signal for a specific amount of time
 """
 
-# duration in µs
-DURATION = 30_000_000
+from typing import List
 
-OUTPUT = [
+TEMPLATE = [
     "Filetype: Flipper SubGhz RAW File",
     "Version: 1",
     "Frequency: 433920000",
@@ -13,30 +12,37 @@ OUTPUT = [
 ]
 
 # WeLl aCtUaLlY iT'S 2,147,483,647
-MAX_VALUE = 2_000_000_000
+MAX_TIMINGS_VALUE = 2_000_000_000
 # that's 2_000_000_000 µs
 # that's 2_000_000 ms
 # that's 2_000 s
 # you get the idea
 
 # max 512 values per line
+# https://github.com/DarkFlippers/unleashed-firmware/blob/dev/documentation/file_formats/SubGhzFileFormats.md#raw-files
 CHUNK_SIZE = 512
 
-# what duration we've already added
-current_duration = 0
-values = []
+def generate_sleep(duration: int) -> List[str]:
+    # what duration we've already added
+    current_duration = 0
+    values = []
 
-while current_duration < DURATION:
-    current_duration += 1000
-    values.append(1000)
+    output = TEMPLATE[:]
 
-    # remaining time we need to add
-    remaining = min(MAX_VALUE, DURATION - current_duration)
-    current_duration += remaining
-    values.append(-remaining)
+    while current_duration < duration:
+        current_duration += 1000
+        values.append(1000)
 
-for i in range(0, len(values), CHUNK_SIZE):
-    vals = values[i:i + CHUNK_SIZE]
-    OUTPUT.append(f'RAW_Data: {" ".join([str(z) for z in vals])}')
+        # remaining time we need to add
+        remaining = min(MAX_TIMINGS_VALUE, duration - current_duration)
+        current_duration += remaining
+        values.append(-remaining)
 
-print('\n'.join(OUTPUT))
+    for i in range(0, len(values), CHUNK_SIZE):
+        vals = values[i:i + CHUNK_SIZE]
+        output.append(f'RAW_Data: {" ".join([str(z) for z in vals])}')
+
+    return output
+
+if __name__ == "__main__":
+    print('\n'.join(generate_sleep(30_000_000)))
